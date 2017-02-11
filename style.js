3dca03ee29ff0fd6fc37dd4cbc70a79c855531f7
Mobolaji David <davidmo@colorado.edu>
2017-02-10 17:34:15 -0800

    change site to single page site, and add jQuery to stop refresh and improve site speed

--- style.js
+++ style.js
@@ -2,6 +2,42 @@ $(window).ready(function() {
    $('body').hide().fadeIn(300);
 })
 
+$(function() {
+  $('.nav a').click(function() {
+    var $linkClicked = $(this).attr('href');
+    document.location.hash = $linkClicked;
+      if (!$(this).hasClass("active")) {
+        $(".nav a").removeClass("active");
+        $(this).addClass("active");
+        $('#info section').hide();
+        $($linkClicked).fadeIn('slow');
+        return false;
+      }
+      else {
+          return false;
+      }
+  });
+  var hash = window.location.hash;
+  hash = hash.replace(/^#/, '');
+  switch (hash) {
+    case 'page1' :
+      $("#" + hash + "-link").trigger("click");
+    break;
+    case 'page2' :
+        $("#" + hash + "-link").trigger("click");
+        break;
+    case 'page3' :
+        $("#" + hash + "-link").trigger("click");
+        break;
+    case 'page4' :
+        $("#" + hash + "-link").trigger("click");
+        break;
+    case 'page5' :
+        $("#" + hash + "-link").trigger("click");
+        break;
+  }
+});
+
 // $(document).ready(function(){
 //     $('.navbar a').on('click', function() {
 //       //remove all pre-existing active classes
